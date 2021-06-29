516d237357066e146840b32fcd5cef1716d0a7d0
artem <artem@wsb-195-72a.icx.local>
2021-06-29 17:02:05 +0300

    Create VPC and subnets

--- providers.tf
+++ providers.tf
@@ -1,3 +1,12 @@
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-bucket"
+    key    = "state-bucket/terraform_state.tfstate"
+    region = var.region
+  }
+}
+
 provider "aws" {
     region = var.region
 }
--- variables.tf
+++ variables.tf
@@ -3,12 +3,32 @@ variable "region" {
   default     = "eu-central-1"
 }
 
-variable default_az {
-  type        = string
-  default     = "eu-central-1a"
-}
-
 
 variable "vpc_cidr" {
+  type        = list(string) 
   default     = "[10.0.0.0/20]"
+}
+
+variable availability_zones {
+  type        = list(string)
+  default     = [
+    "eu-central-1a",
+    "eu-central-1b"
+  ]
+}
+
+variable private_subnets {
+  type        = list(string)
+  default     = [
+    "10.1.0.0/24",
+    "10.2.0.0/24"
+  ]
+}
+
+variable public_subnets {
+  type        = list(string)
+  default     = [
+    "10.3.0.0/24",
+    "10.4.0.0/24"
+  ]
 }
